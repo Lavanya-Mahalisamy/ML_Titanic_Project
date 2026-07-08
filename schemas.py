c1dba2033281c25440db5f7b0d3c746cca546084
Lavanya M <mahalisamyarumugam@gmail.com>
2026-07-08 10:31:27 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -2,6 +2,6 @@ from typing import Optional
 from pydantic import BaseModel
 class Passenger(BaseModel):
     pclass:int
-    sex=str
+    sex:str
     age:Optional[float]=None
     fare:Optional[float]=None
